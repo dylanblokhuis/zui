27cc5f216fa151efc755d953afadc08d3e20da94
dylanblokhuis <dylanblokhuis3@hotmail.com>
2024-02-28 11:06:33 +0100

    the api is shaping up

--- src/main.zig
+++ src/main.zig
@@ -4,16 +4,17 @@ const Allocator = std.mem.Allocator;
 /// # remaining stuff
 ///
 /// how to do if statements
-///
-/// how to loop stuff?
 const zui = struct {
     allocator: Allocator,
+
+    // nodes: std.MultiArrayList(ViewNode),
 
     const Self = @This();
 
     pub fn init(allocator: Allocator) !Self {
         return Self{
             .allocator = allocator,
+            // .nodes = std.MultiArrayList(ViewNode){},
         };
     }
 
@@ -23,13 +24,51 @@ const zui = struct {
         child: ?ViewNode = null,
     };
 
-    const ViewNode = struct {};
+    const ViewNode = struct {
+        class: []const u8 = "",
+        on_click: ?*const fn () void = null,
+        children: ?[]const ViewNode = null,
+    };
 
+    /// function to make a singular view node
     pub fn v(self: *Self, props: p) ViewNode {
-        _ = self; // autofix
-        _ = props; // autofix
+        var children: ?[]ViewNode = null;
 
-        return ViewNode{};
+        if (props.child != null) {
+            children = self.allocator.alloc(ViewNode, 1) catch unreachable;
+            if (props.child != null) {
+                children.?[0] = props.child.?;
+            }
+        }
+
+        return ViewNode{
+            .class = props.class,
+            .on_click = props.on_click,
+            .children = children,
+        };
+    }
+
+    /// function to make an array of view nodes
+    pub fn vv(self: *Self, children: []const ViewNode) ViewNode {
+        const nodes = self.allocator.alloc(ViewNode, children.len) catch unreachable;
+        for (children, 0..) |child, index| {
+            nodes[index] = child;
+        }
+
+        return ViewNode{
+            .children = nodes,
+        };
+    }
+
+    pub fn foreach(self: *Self, comptime T: type, cb: *const fn (self: *Self, item: T, index: usize) ViewNode, items: []const T) ViewNode {
+        var children = self.allocator.alloc(ViewNode, items.len) catch unreachable;
+        for (items, 0..) |item, index| {
+            children[index] = cb(self, item, index);
+        }
+
+        return ViewNode{
+            .children = children,
+        };
     }
 
     pub fn fmt(self: *Self, comptime format: []const u8, args: anytype) []u8 {
@@ -37,33 +76,78 @@ const zui = struct {
     }
 };
 
-const Hello = struct {
-    pub fn handle_click() void {
-        std.log.debug("clicked", .{});
-    }
+// const Hello = struct {
+//     pub fn handle_click() void {
+//         std.log.debug("clicked", .{});
+//     }
 
-    pub fn render(ui: *zui) zui.ViewNode {
-        return ui.v(.{
-            .class = "flex flex-wrap",
-            .on_click = handle_click,
-            .child = ui.v(.{
-                .class = ui.fmt("flex flex-wrap", .{}),
-            }),
-        });
-    }
-};
+//     pub fn render(ui: *zui) []zui.ViewNode {
+//         return ui.v(.{
+//             .class = "flex flex-wrap",
+//             .on_click = handle_click,
+//             .children = ui.vv(.{
+//                 ui.v(.{
+//                     .class = "flex flex-wrap",
+//                 }),
+//                 // .class = "flex flex-wrap",
+//             }),
+//         });
+//     }
+// };
+
+fn forl(ui: *zui, item: u8, index: usize) zui.ViewNode {
+    return zui.ViewNode{
+        .class = ui.fmt("item-{d}-index-{d}", .{ item, index }),
+    };
+}
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    var ui = try zui.init(allocator);
-    const p = zui.p;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var ui = try zui.init(arena.allocator());
 
-    std.log.debug("{any}", .{@sizeOf(zui.p)});
-
-    _ = ui.v(p{
+    const tree = ui.v(.{
         .class = "flex flex-wrap",
-        .child = Hello.render(&ui),
+        .child = ui.vv(&.{
+            ui.v(.{
+                .class = "flex flex-wrap p-4",
+            }),
+            ui.v(.{
+                .class = "flex flex-wrap p-10",
+                .child = ui.foreach(u8, forl, &.{
+                    4,
+                    2,
+                }),
+            }),
+            ui.v(.{
+                .class = ui.fmt("bg-{s}", .{"red-600"}),
+            }),
+        }),
     });
+
+    const PrintTree = struct {
+        const Self = @This();
+
+        pub fn print_tree(node: *const zui.ViewNode, depth: u8) void {
+            for (0..depth) |_| {
+                std.debug.print("  ", .{});
+            }
+            std.debug.print("{s}\n", .{node.class});
+
+            if (node.children == null) {
+                return;
+            }
+
+            // const children = &node.children.?;
+            for (node.children.?) |*child| {
+                Self.print_tree(child, depth + 1);
+            }
+        }
+    };
+
+    PrintTree.print_tree(&tree, 0);
+
+    // arena size
+    std.debug.print("bytes in arena: {d}\n", .{arena.queryCapacity()});
 
     // const props = zui.Props{ .class = "yo" };
     // try ui.v("flex flex-warp", .{});
